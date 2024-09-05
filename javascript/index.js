256dd62eb0950f50c0583c6043187bec12255536
eyad09268 <eyad09268@gmail.com>
2024-09-05 15:12:21 +0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+// // string
+// // var name = "Eyad";
+// // number
+// var age = 14;
+// // boolean
+// var isAdult = true;
+// // null
+// var car = null;
+// // undefined
+// var house ;
+// // object
+// var person = {
+//     username: "eyad",
+//     age: 14,
+// }
+// //array
+// var number = ["1", "2", "3"]
+
+//console
+console.log(`your name is ${name}`);
+console.log(`your age is ${age}`);
+// console.log(`are you an adult ${isAdult}`);
+//document
+var name = prompt("what is your name");
+var age = prompt("what is your age");
+document.getElementById("title").innerHTML = `hello ${name} you are ${age} years old`;
